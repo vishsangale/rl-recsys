6b21c78fc344c47f2202df3a698172dccc0caddf
Vishwanath Sangale <vishsangale@gmail.com>
2026-05-06 21:49:41 -0700

    chore: export swis_value, dr_value, VarianceEvaluation from evaluation package

--- rl_recsys/evaluation/__init__.py
+++ rl_recsys/evaluation/__init__.py
@@ -3,17 +3,24 @@
 from rl_recsys.evaluation.ope import (
     OPEEvaluation,
     OPERecord,
+    dr_value,
     evaluate_ope_agent,
     ips_value,
     replay_value,
     snips_value,
+    swis_value,
 )
+from rl_recsys.evaluation.variance import VarianceEvaluation, evaluate_with_variance
 
 __all__ = [
     "OPEEvaluation",
     "OPERecord",
+    "dr_value",
     "evaluate_ope_agent",
     "ips_value",
     "replay_value",
     "snips_value",
+    "swis_value",
+    "VarianceEvaluation",
+    "evaluate_with_variance",
 ]
