41742d4e31314bccca568714cf70ef5a7f2c9088
Vishwanath Sangale <vishsangale@gmail.com>
2026-05-06 22:57:47 -0700

    chore: export trajectory evaluation symbols from evaluation package

--- rl_recsys/evaluation/__init__.py
+++ rl_recsys/evaluation/__init__.py
@@ -10,17 +10,34 @@ from rl_recsys.evaluation.ope import (
     snips_value,
     swis_value,
 )
-from rl_recsys.evaluation.variance import VarianceEvaluation, evaluate_with_variance
+from rl_recsys.evaluation.trajectory import (
+    Session,
+    TrajectoryDataset,
+    TrajectoryEvaluation,
+    TrajectoryStep,
+    evaluate_trajectory_agent,
+)
+from rl_recsys.evaluation.variance import (
+    VarianceEvaluation,
+    evaluate_trajectory_with_variance,
+    evaluate_with_variance,
+)
 
 __all__ = [
     "OPEEvaluation",
     "OPERecord",
+    "Session",
+    "TrajectoryDataset",
+    "TrajectoryEvaluation",
+    "TrajectoryStep",
+    "VarianceEvaluation",
     "dr_value",
     "evaluate_ope_agent",
+    "evaluate_trajectory_agent",
+    "evaluate_trajectory_with_variance",
+    "evaluate_with_variance",
     "ips_value",
     "replay_value",
     "snips_value",
     "swis_value",
-    "VarianceEvaluation",
-    "evaluate_with_variance",
 ]
