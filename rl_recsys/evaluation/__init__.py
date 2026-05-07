c720bfb6bd51ccf0e64bc14565a012b63e4881e1
Vishwanath Sangale <vishsangale@gmail.com>
2026-05-07 00:03:01 -0700

    chore: export sequential DR symbols from evaluation package

--- rl_recsys/evaluation/__init__.py
+++ rl_recsys/evaluation/__init__.py
@@ -10,6 +10,13 @@ from rl_recsys.evaluation.ope import (
     snips_value,
     swis_value,
 )
+from rl_recsys.evaluation.ope_trajectory import (
+    LoggedTrajectoryStep,
+    LoggedTrajectorySource,
+    TrajectoryOPEEvaluation,
+    evaluate_trajectory_ope_agent,
+    seq_dr_value,
+)
 from rl_recsys.evaluation.trajectory import (
     Session,
     TrajectoryDataset,
@@ -24,20 +31,25 @@ from rl_recsys.evaluation.variance import (
 )
 
 __all__ = [
+    "LoggedTrajectorySource",
+    "LoggedTrajectoryStep",
     "OPEEvaluation",
     "OPERecord",
     "Session",
     "TrajectoryDataset",
     "TrajectoryEvaluation",
+    "TrajectoryOPEEvaluation",
     "TrajectoryStep",
     "VarianceEvaluation",
     "dr_value",
     "evaluate_ope_agent",
     "evaluate_trajectory_agent",
+    "evaluate_trajectory_ope_agent",
     "evaluate_trajectory_with_variance",
     "evaluate_with_variance",
     "ips_value",
     "replay_value",
+    "seq_dr_value",
     "snips_value",
     "swis_value",
 ]
